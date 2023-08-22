76d2a66aa2e10ffabbc71b38bd45fe0464f21e08
nitesh-19 <niteshgohil25@gmail.com>
2023-08-22 18:36:23 +0530

    Added feature where user has to guess the state highlighted randomly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from screen_handler import ScreenHandler
+import random
 
 MAP_DATA_DICT = {'Andhra Pradesh': (-113.0, -200.0), 'Arunachal Pradesh': (327.0, 194.0), 'Assam': (271.0, 132.0),
                  'Bihar': (87.0, 104.0), 'Chhattisgarh': (-20.0, -9.0), 'Goa': (-239.0, -187.0),
@@ -10,22 +11,38 @@ MAP_DATA_DICT = {'Andhra Pradesh': (-113.0, -200.0), 'Arunachal Pradesh': (327.0
                  'Sikkim': (152.0, 162.0), 'Tamil Nadu': (-122.0, -307.0), 'Telangana': (-96.0, -121.0),
                  'Tripura': (240.0, 53.0), 'Uttar Pradesh': (-36.0, 128.0), 'Uttarakhand': (-92.0, 238.0),
                  'West Bengal': (135.0, 36.0)}
-
+states_list = list(MAP_DATA_DICT.keys())
+random.shuffle(states_list)
+print(states_list)
 screen = ScreenHandler()
 game_over = False
-
+states_list_index = 0
 states_guessed = []
+skipped = []
 while not game_over:
+    if states_list_index >= len(states_list):
+        print(f"Game Over! Your Score: {len(states_guessed)}")
+        break
+    coordinates = MAP_DATA_DICT[states_list[states_list_index]]
+    screen.place_dot(coordinates)
+    screen.display.update()
     response = screen.prompt(len(states_guessed))
     print(f"Last input: {response}")
     if response in states_guessed:
         print("Already guessed! Try a different one.")
-    elif response in MAP_DATA_DICT:
+
+    elif response == states_list[states_list_index]:
         screen.write_text(text=response, coordinates=MAP_DATA_DICT[response])
         states_guessed.append(response)
+        states_list_index += 1
+
         if len(states_guessed) == len(MAP_DATA_DICT):
             print("All states guessed. You Win!")
             game_over = True
+    elif response == "Skip":
+        skipped.append(states_list[states_list_index])
+        states_list_index += 1
+        continue
     elif response == 'Exit':  # If user types 'Exit' then export all the states they missed to guess in a file.
         with open("missed_states.csv", "w") as missed_file:
             for key in MAP_DATA_DICT:
@@ -33,7 +50,9 @@ while not game_over:
                     pass
                 else:
                     missed_file.write(f"{key} \n")
+        print(f"Game Over! Your Score: {len(states_guessed)}")
         game_over = True
+
     else:
         print("Wrong input, check your spelling.")
 
